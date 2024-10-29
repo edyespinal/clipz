075e5d89716a0b372cadfd19b4fc447b44d8b201
Edy Espinal <edyespinal@gmail.com>
2024-10-29 23:03:01 +0100

    Add Firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,28 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from "@angular/core";
+import { provideRouter, withViewTransitions } from "@angular/router";
 
-import { routes } from './app.routes';
+import { routes } from "./app.routes";
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { getAuth, provideAuth } from "@angular/fire/auth";
+import { getFirestore, provideFirestore } from "@angular/fire/firestore";
+import { getDatabase, provideDatabase } from "@angular/fire/database";
+import { getStorage, provideStorage } from "@angular/fire/storage";
+import { provideAnimations } from "@angular/platform-browser/animations";
+import { environment } from "../environments/environment.development";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withViewTransitions()),
+    provideAnimations(),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideStorage(() => getStorage()),
+  ],
 };
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebaseConfig: {
+    projectId: "clipz-game",
+    appId: "1:656225951785:web:f9d1cc545fdbec4e333c38",
+    storageBucket: "clipz-game.appspot.com",
+    apiKey: "AIzaSyCjQEpweP5u2QsJtu1lfBb5zqsrtlqL364",
+    authDomain: "clipz-game.firebaseapp.com",
+    messagingSenderId: "656225951785",
+  },
+};
