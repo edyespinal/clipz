dedc4e2c10c3b14fefea89415fd93a7cc046c919
Edy Espinal <edyespinal@gmail.com>
2024-10-30 13:35:59 +0100

    Update game components

--- src/app/components/game/game-header/game-header.component.ts
+++ src/app/components/game/game-header/game-header.component.ts
@@ -1,13 +1,13 @@
 import { Component, inject, input, signal } from "@angular/core";
 import { ButtonComponent } from "../../common/button/button.component";
-import { AsyncPipe, NgClass } from "@angular/common";
+import { AsyncPipe } from "@angular/common";
 import { GameService } from "@services/game.service";
 import { HeartComponent } from "../heart/heart.component";
 
 @Component({
   selector: "app-game-header",
   standalone: true,
-  imports: [ButtonComponent, NgClass, AsyncPipe, HeartComponent],
+  imports: [ButtonComponent, AsyncPipe, HeartComponent],
   templateUrl: "./game-header.component.html",
   styleUrl: "./game-header.component.css",
 })
--- src/app/components/game/heart/heart.component.ts
+++ src/app/components/game/heart/heart.component.ts
@@ -9,13 +9,4 @@ import { Component, input, signal } from "@angular/core";
 })
 export class HeartComponent {
   fill = input();
-  style = signal("");
-
-  ngOnChanges() {
-    this.style.set(
-      this.fill() === true
-        ? "fill: rgb(236, 0, 32); stroke: black; stroke-width: 4.38px"
-        : "fill: rgb(255, 255, 255); stroke: black; stroke-width: 4.38px",
-    );
-  }
 }
