8179db13aa49aaa55a5051aee89dc29d7df6e4cd
Edy Espinal <edyespinal@gmail.com>
2024-11-01 10:06:32 +0100

    chore: add loading state to leader board

--- src/app/pages/leader-board/leader-board.component.ts
+++ src/app/pages/leader-board/leader-board.component.ts
@@ -5,6 +5,7 @@ import { LeaderBoardService } from "@services/leader-board.service";
 import { Leader } from "@models/Leader";
 import { GetLeaderMedalPipe } from "@pipes/get-leader-medal.pipe";
 import { MainLayoutComponent } from "../../components/layout/main-layout/main-layout.component";
+import { LoaderComponent } from "../../components/common/loader/loader.component";
 
 @Component({
   selector: "app-leader-board",
@@ -14,16 +15,19 @@ import { MainLayoutComponent } from "../../components/layout/main-layout/main-la
     ButtonComponent,
     GetLeaderMedalPipe,
     MainLayoutComponent,
+    LoaderComponent,
   ],
   templateUrl: "./leader-board.component.html",
 })
 export class LeaderBoardComponent implements OnInit {
   leaderBoardService = inject(LeaderBoardService);
   leaders = signal<Leader[]>([]);
+  isLoading = signal(true);
 
   async ngOnInit() {
     const leaders = await this.leaderBoardService.getLeaders();
 
     this.leaders.set(leaders);
+    this.isLoading.set(false);
   }
 }
